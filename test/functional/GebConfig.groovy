faccd8ed692b85d54f764b78e192886e2425d9e7
Miles Burton <miles@mnetcs.com>
2012-06-05 15:38:25 +0100

    @Miles adding initial saucelabs support

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,2 +1,17 @@
 baseUrl = 'http://localhost:8080/whatsinmyhandbag-twitter/'
 
+sauce {
+	baseUrl = 'http://myapp.test/whatsinmyhandbag-twitter/'
+	String username = System.getenv("SAUCE_ONDEMAND_USERNAME");
+	String apiKey = System.getenv("SAUCE_ONDEMAND_ACCESS_KEY");
+	if(username == null || apiKey == null){
+	    System.err.println("Sauce OnDemand credentials not set.");
+	}
+	
+	DesiredCapabilities capabillities = DesiredCapabilities.chrome();
+	capabillities.setCapability("name", "whatsinmyhandbag-twitter");
+	capabillities.setCapability("platform", Platform.LINUX);
+	capabillities.setCapability("selenium-version", "2.21.0");
+
+	driver = { new RemoteWebDriver(new URL("http://${username}:${apiKey}@ondemand.saucelabs.com:80/wd/hub"), capabillities) }
+}
